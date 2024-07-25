68615ce24fead53170f3b1d7b5a63414dcee4b2a
Astrokke <nathan@musielak.net>
2024-07-25 16:01:46 +0200

    premier version du template dashboard

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Entity\Task;
+use App\Repository\ProjectRepository;
+use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,27 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-
-    #[Route('/dashboard', name: 'app_dashboard')]
-    public function dashboard(EntityManagerInterface $entityManager): Response
+  
     
-    {
 
-        $user = $this->getUser();
-        $tasks = $entityManager->getRepository(Task::class)->findByUser($user);
-        $projects = $entityManager->getRepository(Project::class)->findByUser($user);
+    #[Route('/', name: 'app_dashboard')]
+    public function index(TaskRepository $taskRepository, ProjectRepository $projectRepository): Response
+    {
+        $tasks = $taskRepository->findAll();
+        $projects = $projectRepository->findAll();
 
         return $this->render('user/dashboard.html.twig', [
             'tasks' => $tasks,
             'projects' => $projects,
         ]);
-      
     }
 }
