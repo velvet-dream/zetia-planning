4a4b5d9415db390ebb46ac77003784dab3ce21a0
Velvet <62705392+velvet-dream@users.noreply.github.com>
2024-10-22 18:11:23 +0200

    close alert when closing button is clicked

--- assets/scripts/components/initializeAlerts.ts
+++ assets/scripts/components/initializeAlerts.ts
@@ -1,6 +1,12 @@
 function initializeAlerts() {
     const alerts: NodeListOf<HTMLElement> = document.querySelectorAll(".zetia-alert")
     for (const alert of alerts) {
+        const closeButton = alert.querySelector(".zetia-alert-header .zetia-button")
+
+        if (closeButton) {
+            closeButton.addEventListener("click", () => { alert.remove() })
+        } else console.error('No closing button defined on', alert)
+
         // timeout in s before alert must be removed
         const timeoutValue = parseFloat(alert.dataset['timeout'] ?? '0')
 
