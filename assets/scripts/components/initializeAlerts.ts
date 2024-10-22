763a90a9908c2e53a7708aabf1ce0a7502a65f7f
Velvet <62705392+velvet-dream@users.noreply.github.com>
2024-10-22 18:05:08 +0200

    data timeout

--- assets/scripts/components/initializeAlerts.ts
+++ assets/scripts/components/initializeAlerts.ts
@@ -1,4 +1,20 @@
 function initializeAlerts() {
+    const alerts: NodeListOf<HTMLElement> = document.querySelectorAll(".zetia-alert")
+    for (const alert of alerts) {
+        // timeout in s before alert must be removed
+        const timeoutValue = parseFloat(alert.dataset['timeout'] ?? '0')
+        console.log(timeoutValue)
+
+        if (timeoutValue <= 0) continue
+
+        setTimeout(() => {
+            // adding this class makes the alert go byebye
+            alert.classList.add("zetia-alert-closing")
+            setTimeout(() => {
+                alert.remove()
+            }, 1000)
+        }, timeoutValue * 1000)
+    }
 }
 
 initializeAlerts()
