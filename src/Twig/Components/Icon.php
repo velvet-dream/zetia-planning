945b59f26dab2c24526201af857ea9be779c1252
Velvet <62705392+velvet-dream@users.noreply.github.com>
2024-10-23 14:19:33 +0200

    implement default alt text on icons

--- src/Twig/Components/Icon.php
+++ src/Twig/Components/Icon.php
@@ -9,6 +9,15 @@ interface IconImage
 {
     const SETTINGS = 'settings';
     const ZETIA = 'zetia-logo';
+    const CROSS = 'close';
+    const DASHBOARD = 'dashboard';
+    const TRASH = 'delete';
+    const VALID = 'done';
+    const PENCIL = 'edit';
+    const EYE = 'eye';
+    const LOGOUT = 'logout';
+    const SEARCH = 'search';
+    const TEAM = 'team';
 }
 
 interface IconVariant
@@ -24,18 +33,45 @@ class Icon
     // to pass an href as prop, use {{ path('app_index') }} in a template
     public string $href;
     public string $variant = IconVariant::DEFAULT;
-    public string $altText;
+    public string $altText = "";
 
     #[PostMount]
     public function postMount(): void
     {
-        if (!is_null($this->icon) && is_null($this->altText)) {
+        if (!is_null($this->icon) && $this->altText === "") {
             switch ($this->icon) {
                 case IconImage::SETTINGS:
                     $this->altText = 'Réglages';
                     break;
                 case IconImage::ZETIA:
                     $this->altText = 'Le logo de Zetia Planning';
+                    break;
+                case IconImage::CROSS:
+                    $this->altText = 'Fermer';
+                    break;
+                case IconImage::DASHBOARD:
+                    $this->altText = 'Tableau de bord';
+                    break;
+                case IconImage::TRASH:
+                    $this->altText = 'Supprimer';
+                    break;
+                case IconImage::VALID:
+                    $this->altText = 'Valider';
+                    break;
+                case IconImage::PENCIL:
+                    $this->altText = 'Éditer';
+                    break;
+                case IconImage::EYE:
+                    $this->altText = 'Consulter';
+                    break;
+                case IconImage::LOGOUT:
+                    $this->altText = 'Se déconnecter';
+                    break;
+                case IconImage::SEARCH:
+                    $this->altText = 'Rechercher';
+                    break;
+                case IconImage::TEAM:
+                    $this->altText = "L'équipe";
                     break;
             }
         }
