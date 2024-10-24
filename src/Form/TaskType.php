ae3a477b9478665c2535c3a9fe26fb14a3e015a5
Velvet <62705392+velvet-dream@users.noreply.github.com>
2024-10-24 10:12:40 +0200

    use default form template (almost) everywhere, refactoring

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -15,20 +15,28 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('tskTitle')
+            ->add('tskTitle', null, [
+                'label' => 'Titre',
+            ])
+            ->add('tskDescription', null, [
+                'label' => 'Description',
+            ])
             ->add('tskDateDebut', null, [
                 'widget' => 'single_text',
+                'label' => 'Date de début',
             ])
             ->add('tskDateFinPrevisionnelle', null, [
+                'label' => 'Date de fin prévue',
                 'widget' => 'single_text',
             ])
-            ->add('tskDescription')
             ->add('tskStatus', EntityType::class, [
+                'label' => 'Statut',
                 'class' => StatusTask::class,
                 'choice_label' => 'stkTitle',
-                'placeholder' => 'Choisir un statut',
+                'placeholder' => 'Choisissez un statut',
             ])
             ->add('project', EntityType::class, [
+                'label' => 'Projet',
                 'class' => Project::class,
                 'choice_label' => 'pctTitle',
                 'placeholder' => 'Sélectionner un projet',
