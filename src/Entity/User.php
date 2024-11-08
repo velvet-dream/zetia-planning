ceb378bb88def1dd6f5176975c35c7cfecb1dbaf
Velvet <62705392+velvet-dream@users.noreply.github.com>
2024-11-08 17:57:22 +0100

    usr_mail can't be recognized

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'user_usr')]
-#[UniqueEntity(fields: ['usr_mail'], message: 'There is already an account with this email adress')]
+#[UniqueEntity(fields: ['usrMail'], message: 'There is already an account with this email adress')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
